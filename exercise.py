3c0c1900bb0526f93ed1daa123d0b0399688f7d1
Terroirman <jerome.gagnon@gmail.com>
2021-08-28 21:46:32 -0400

    reformat

--- exercise.py
+++ exercise.py
@@ -1,8 +1,8 @@
 class Bottles(object):
 
     plural: dict = {
-        0: "",
-        1: ""
+        0: '',
+        1: ''
     }
 
     def verse(self, verse_num: int):
@@ -11,9 +11,9 @@ class Bottles(object):
     @staticmethod
     def phrase1(bottle_num: int):
         s = Bottles.plural.get(bottle_num, 's')
-        return f"{bottle_num} bottle{s} of beer on the wall, {bottle_num} bottle{s} of beer."
+        return f'{bottle_num} bottle{s} of beer on the wall, {bottle_num} bottle{s} of beer.'
 
     @staticmethod
     def phrase2(bottle_num: int):
         s = Bottles.plural.get(bottle_num, 's')
-        return f"Take one down and pass it around, {bottle_num} bottle{s} of beer on the wall."
+        return f'Take one down and pass it around, {bottle_num} bottle{s} of beer on the wall.'
