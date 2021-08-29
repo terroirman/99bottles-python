423b36dccd98a423da0b4d7d834f9bc4acee0359
Terroirman <jerome.gagnon@gmail.com>
2021-08-28 21:45:40 -0400

    test_verse_2

--- exercise.py
+++ exercise.py
@@ -1,9 +1,19 @@
 class Bottles(object):
-    def verse(self, verse_num: int):
-        return (
-            f"{verse_num} bottles of beer on the wall, {verse_num} bottles of beer.\n"
-            f"Take one down and pass it around, {verse_num-1} bottles of beer on the wall."
-        )
 
-    def phrase(self, bottle_num: int, phrase: str):
-        return None
+    plural: dict = {
+        0: "",
+        1: ""
+    }
+
+    def verse(self, verse_num: int):
+        return Bottles.phrase1(verse_num) + '\n' + Bottles.phrase2(verse_num-1)
+
+    @staticmethod
+    def phrase1(bottle_num: int):
+        s = Bottles.plural.get(bottle_num, 's')
+        return f"{bottle_num} bottle{s} of beer on the wall, {bottle_num} bottle{s} of beer."
+
+    @staticmethod
+    def phrase2(bottle_num: int):
+        s = Bottles.plural.get(bottle_num, 's')
+        return f"Take one down and pass it around, {bottle_num} bottle{s} of beer on the wall."
